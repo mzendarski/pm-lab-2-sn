feb20ed691bcd52c8edc2fea0002335a8ad18de9
Mikołaj <mikolaj.zendarski@student.wat.edu.pl>
2020-11-30 11:41:52 +0100

    Wykonano zadnie 2.3.2 - register IRQ

--- Zadanie-2-3/src/main.cpp
+++ Zadanie-2-3/src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+volatile int state = LOW;
+ int main() {
+   init(); // Inicjalizacja biblioteki Arduino
+   DDRB|= B00110000; // Konfiguracja portu PB4 i PB5 (D12 i D13) jako wyjście
+   PORTD |= (1 << PORTD2); // Włączenie portu PD2 dla INT0
+   EICRA |= (1 << ISC00); //Wykrywanie każdej zmiany stanu logicznego
+   EIMSK |= (1 << INT0); //Włączenie przerwania INT0
+   sei(); //Globalne włączenie przerwań
+   while (1) {
+     PORTB |= (1 << PORTB5);
+     delay(500);
+     PORTB &= !(1 << PORTB5);
+     delay(500);
+   }
+ }
+ 
+ ISR(INTO_vect){
+   state = !state;
+   digitalWrite(12, state);
+ }
